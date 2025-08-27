c820b5cab6e832be85f70d50aebbc4e96cc56910
iimomo <epotetoco3@gmail.com>
2025-08-27 16:37:14 +0900

    TOP微調整

--- app/components/pages/top.tsx
+++ app/components/pages/top.tsx
@@ -26,22 +26,24 @@ export const TopPage: FC<Props> = ({ data }) => {
           <p>
             {data.month.slice(0, 4)}年{data.month.slice(4, 6)}月分のお支払額
           </p>
-          <p>・合計金額：{data.spending.toLocaleString()}円</p>
-          <p>
-            ・1人目金額：
-            {calculatePersonAmount(data.spending, true).toLocaleString()}円
-          </p>
-          <p>
-            ・2人目金額：{calculatePersonAmount(data.spending).toLocaleString()}
-            円
-          </p>
+          <ul>
+            <li>合計金額：{data.spending.toLocaleString()}円</li>
+            <li>
+              1人目金額：
+              {calculatePersonAmount(data.spending, true).toLocaleString()}円
+            </li>
+            <li>
+              2人目金額：{calculatePersonAmount(data.spending).toLocaleString()}
+              円
+            </li>
+          </ul>
 
           <div>
             内訳
             <ul>
               <li>単価：{(data.spending / 2).toLocaleString()}円</li>
               <li>
-                家賃：{RENT_AMOUNT.toLocaleString()} 〜
+                家賃：{RENT_AMOUNT.toLocaleString()} 〜{' '}
                 {(RENT_AMOUNT + RENT_DIFFERENCE).toLocaleString()}円
               </li>
               <li>貯金：{SAVINGS_AMOUNT.toLocaleString()}円</li>
