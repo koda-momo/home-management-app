f467700866a2550a337c4f36beda897b2278038c
iimomo <epotetoco3@gmail.com>
2025-08-31 17:12:59 +0900

    デザイン調整

--- app/components/layout/Footer/Footer.css.ts
+++ app/components/layout/Footer/Footer.css.ts
@@ -2,11 +2,9 @@ import { style } from '@vanilla-extract/css';
 import { themeColor } from '~/styles/const';
 
 export const background = style({
-  width: '100%',
+  height: 100,
   backgroundColor: themeColor.secondary,
   padding: '10px 10px',
-  position: 'fixed',
-  bottom: 0,
 });
 
 export const link = style({
--- app/components/layout/PageLayout/PageLayout.css.ts
+++ app/components/layout/PageLayout/PageLayout.css.ts
@@ -3,10 +3,11 @@ import { themeColor } from '~/styles/const';
 
 export const main = style({
   width: '100%',
-  height: '100vh',
-  background: themeColor.background,
+  minHeight: '100vh',
 });
 
 export const pageLayout = style({
+  minHeight: 'calc(100vh - 150px)', // Header, Footer
+  background: themeColor.background,
   padding: '20px 10px',
 });
