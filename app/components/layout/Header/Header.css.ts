3a23037fec5d21891df0e6e287fc70bd270dcfd1
iimomo <epotetoco3@gmail.com>
2025-08-31 17:41:07 +0900

    SP Headerメニュー追加

--- app/components/layout/Header/Header.css.ts
+++ app/components/layout/Header/Header.css.ts
@@ -2,6 +2,7 @@ import { style } from '@vanilla-extract/css';
 import { themeColor } from '~/styles/const';
 
 export const background = style({
+  position: 'relative',
   height: 50,
   backgroundColor: themeColor.secondary,
   padding: '0 10px',
@@ -14,7 +15,12 @@ export const logo = style({
   fontWeight: 'bold',
 });
 
-export const linkList = style({
+export const pcLinkList = style({
+  '@media': {
+    'screen and (max-width: 768px)': {
+      display: 'none',
+    },
+  },
   marginLeft: 'auto',
   display: 'flex',
   gap: 10,
@@ -26,4 +32,37 @@ export const link = style({
   ':hover': {
     color: themeColor.text,
   },
+});
+
+export const spLinkButton = style({
+  '@media': {
+    'screen and (min-width: 768px)': {
+      display: 'none',
+    },
+  },
+  position: 'absolute',
+  top: 10,
+  right: 0,
+  zIndex: 10,
+  fontSize: 30,
+  color: themeColor.subText,
+  border: 'none',
+  background: themeColor.secondary,
+});
+
+export const spLinkList = style({
+  background: themeColor.secondary,
+  width: 200,
+  height: 'calc(100vh - 50px)',
+  position: 'absolute',
+  top: 50,
+  right: 0,
+  padding: 10,
+});
+
+export const spLink = style({
+  display: 'block',
+  padding: '10px 0',
+  color: themeColor.subText,
+  textDecoration: 'none',
 });
--- app/components/layout/Header/Header.tsx
+++ app/components/layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import { useState, useEffect, useRef, type FC } from 'react';
 import { Link } from 'react-router';
 import { path } from '~/utils/const';
 import * as styles from './Header.css';
@@ -11,17 +11,61 @@ interface HeaderProps {
  * Header.
  */
 export const Header: FC<HeaderProps> = ({ isLogin = false }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setMenuOpen(false);
+      }
+    };
+
+    if (menuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuOpen]);
+
   return (
     <div className={styles.background}>
       <div className={styles.logo}>家庭管理システム</div>
+
+      {/* PC */}
       {!isLogin && (
-        <div className={styles.linkList}>
-          {path.map(({ label, link }) => (
-            <Link key={label} to={link} className={styles.link}>
-              {label}
-            </Link>
-          ))}
-        </div>
+        <>
+          <div className={styles.pcLinkList}>
+            {path.map(({ label, link }) => (
+              <Link key={label} to={link} className={styles.link}>
+                {label}
+              </Link>
+            ))}
+          </div>
+
+          {/* SP */}
+          <div ref={menuRef}>
+            <button
+              className={styles.spLinkButton}
+              onClick={() => {
+                setMenuOpen(!menuOpen);
+              }}
+            >
+              {menuOpen ? '×' : '≡'}
+            </button>
+            {menuOpen && (
+              <div className={styles.spLinkList}>
+                {path.map(({ label, link }) => (
+                  <Link key={label} to={link} className={styles.spLink}>
+                    {label}
+                  </Link>
+                ))}
+              </div>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
